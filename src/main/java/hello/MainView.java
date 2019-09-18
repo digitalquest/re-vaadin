1aa75864f67ccadd63ce4fb99be5ce542efe551a
sammy <s4mmy@gmx.com>
2019-09-18 21:29:13 +0100

    start adding form

--- src/main/java/hello/MainView.java
+++ src/main/java/hello/MainView.java
@@ -1,11 +1,14 @@
 package hello;
 
+import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.upload.Upload;
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer;
 import com.vaadin.flow.data.value.ValueChangeMode;
 import com.vaadin.flow.router.Route;
 import org.apache.commons.lang3.StringUtils;
@@ -30,8 +33,18 @@ public class MainView extends VerticalLayout {
         this.filter = new TextField();
         this.addNewBtn = new Button("New customer", VaadinIcon.PLUS.create());
 
+        HorizontalLayout fileUploadBox = new HorizontalLayout();
+        MemoryBuffer buffer = new MemoryBuffer();
+        Upload upload = new Upload(buffer);
+        upload.addSucceededListener(event -> {
+            Component component = createComponent(event.getMIMEType(),
+                    event.getFileName(), buffer.getInputStream());
+            showOutput(event.getFileName(), component, output);
+        });
+        fileUploadBox.add(upload);
+
         HorizontalLayout actions = new HorizontalLayout(filter, addNewBtn);
-        add(actions, grid, editor);
+        add(upload, actions, grid, editor);
 
         grid.setHeight("300px");
         grid.setColumns("id", "firstName", "lastName");
